16a7be9bf0bd3c95bc42fb8235b5f1f7497d2841
JonathanYin <joyin@ucsd.edu>
2022-10-24 14:40:37 -0700

    Added comments for stringparser.cpp

--- StringParser.cpp
+++ StringParser.cpp
@@ -16,13 +16,20 @@ int main() {
     vector<string> out;
 
     while ((end = text.find(delim, start)) != string::npos) {
+        // set end = index of delimiter, check if not equal to end of string
         token = text.substr(start, end - start);
+        // set token to string before delimiter
         start = end + delim.length();
+        // set start to new position
         out.push_back(token);
+        // add string to vector
     }
+
     out.push_back(text.substr(start));
+    // add final string
+    
     for (int i = 0; i < out.size(); i++) {
-        cout << out[i] << endl;
+        cout << out[i] << " ";
     }
 
     // int pos = 0;
