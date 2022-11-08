1c12da501131b9809870eb769617a9b65d29f187
JonathanYin <joyin@ucsd.edu>
2022-11-07 18:31:48 -0800

    Added main for testing

--- KthLargestElement.java
+++ KthLargestElement.java
@@ -1,8 +1,14 @@
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class KthLargestElement {
+    public static void main(String[] args){
+        int[] arr = {2, 4, 3, 1, 8, 6};
+        int k = 3;
+        System.out.println(kthLargest(arr, k));
+    }
     public static int kthLargest(int[] arr, int k){
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
         for (int i = 0; i < arr.length; i++){
             pq.add(arr[i]);
