de9930cdae38a3cea90c1f3bec9be8d23b35edf9
JonathanYin <joyin@ucsd.edu>
2023-01-10 11:55:45 -0800

    Add comments

--- ContainerWithMostWater.js
+++ ContainerWithMostWater.js
@@ -3,7 +3,9 @@ var maxArea = function(heights) {
     let left = 0;
     let right = heights.length - 1;
     
+    // while pointers are positioned correctly
     while (left < right) {
+        // calculate area for current container
         const minHeight = Math.min(heights[left], heights[right]);
         const area = minHeight * (right - left);
         
@@ -11,9 +13,12 @@ var maxArea = function(heights) {
             max = area;
         }
         
+        // if left height is the limiting factor, the only way area can be increased is if left height is increased, so increment left pointer
         if (heights[left] < heights[right]) {
             left++;
-        } else {
+        } 
+        // else, increment right pointer
+        else {
             right--;
         }
     }
