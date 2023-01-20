8ddf223f977327f974435fbfe59de0993c418d1f
JonathanYin <joyin@ucsd.edu>
2023-01-19 22:49:34 -0800

    Add comments

--- SumOfTwoIntegers.js
+++ SumOfTwoIntegers.js
@@ -1,7 +1,10 @@
 var getSum = function(a, b) {
     while (b != 0) {
+        // calculate carry
         let tmp = (a & b) << 1;
+        // take XOR of a and b
         a = a ^ b;
+        // set b as the carry-over
         b = tmp;
     }
     return a;
