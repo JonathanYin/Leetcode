6718d78bfdc13c23c3dcd1f3d69cc6373e378a4d
JonathanYin <joyin@ucsd.edu>
2023-09-09 14:24:41 -0700

    Add comments

--- MeetingRooms.js
+++ MeetingRooms.js
@@ -1,3 +1,7 @@
+/*
+ * Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+*/
+
 const canAttendAllMeetings = (meetings) => {
     meetings.sort((a, b) => a[0] - b[0]);
     for (let i = 1; i < meetings.length; i++) {
