35b426aa05fd12f483a0236f2a1031f5f1512fa8
JonathanYin <joyin@ucsd.edu>
2023-09-11 14:18:58 -0700

    Add comments

--- ClimbingStairs.js
+++ ClimbingStairs.js
@@ -4,10 +4,14 @@
  */
 var climbStairs = function(n) {
     let dp = new Array(n + 1).fill(0);
+    // 1 way to climb 0 stairs
     dp[0] = 1;
 
+    // loop through every number of stairs, from 1 to n
     for (let i = 1; i < dp.length; i++) {
+        // we can reach 'i' stairs by climbing 1 step from dp[i - 1]
         dp[i] += dp[i - 1];
+        // if i >= 2, we can reach 'i' stairs by climbing 2 steps from dp[i - 2]
         if (i - 2 >= 0) {
             dp[i] += dp[i - 2];
         }
--- CoinChange.js
+++ CoinChange.js
@@ -5,11 +5,15 @@
  */
 var coinChange = function(coins, amount) {
     let dp = new Array(amount + 1).fill(Infinity);
+    // 0 ways to form 0 cents
     dp[0] = 0;
 
+    // loop through every amount, from 1 to amount
     for (let i = 1; i < dp.length; i++) {
+        // for each coin type, check if it is less than the amount
         for (let coin of coins) {
             if (coin <= i) {
+                // if we can reach 'i - coin' amount with dp[i - coin] coins, then we can reach 'i' amount with dp[i - coin] + 1 coins
                 dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
         }
