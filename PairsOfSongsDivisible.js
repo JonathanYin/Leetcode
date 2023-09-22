eb4b12a5de74b418e7302fb90012b491a5d43fb1
JonathanYin <joyin@ucsd.edu>
2023-09-22 14:31:59 -0700

    Init PairsOfSongsWithTotalDurationsDivisibleBy60 #1010

---
+++ PairsOfSongsDivisible.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} time
+ * @return {number}
+ */
+var numPairsDivisibleBy60 = function(time) {
+    let count = 0;
+    // initialize hashmap to store remainders
+    let map = new Map();
+    for (let i = 0; i < time.length; i++) {
+        let remainder = time[i] % 60;
+        // set complement to the amount needed to dd up to 60
+        let complement = (60 - remainder) % 60;
+        // if the complement exists in the map, add the number of times it appears to the count
+        if (map.has(complement)) {
+            count += map.get(complement);
+        }
+        // add the remainder to the map, or increment its count
+        map.set(remainder, map.get(remainder) + 1 || 1);
+    }
+    return count;
+};
