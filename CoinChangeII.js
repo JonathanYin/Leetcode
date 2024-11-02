2c319edb165bd96fa2ba1909488609238d0dd6ea
JonathanYin <joyin@ucsd.edu>
2024-11-01 21:03:03 -0700

    Init #518

---
+++ CoinChangeII.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+	let dp = new Array(amount + 1).fill(0);
+	dp[0] = 1;
+
+	for (let coin of coins) {
+		for (let i = coin; i <= amount; i++) {
+			dp[i] += dp[i - coin];
+		}
+	}
+	return dp[amount];
+};
